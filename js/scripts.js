f6bf5cc99223020f7bbad366086acdbb6d1af5f0
Dan Mooar <50853302+JubJubby@users.noreply.github.com>
2024-03-21 20:34:18 -0400

    cloned my work from this exercise

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// //name exercise
+// let myName = 'Bob';
+// document.write(myName);
+// myName = 'John Doe';
+// document.write(myName);
+
+// //example 1
+// let simpleAddition = 2 + 2;
+// document.write(simpleAddition);
+
+// //example 2
+// let size = 100;
+// let doubleSize = size * 2;
+// document.write(doubleSize);
+
+//example 3
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
